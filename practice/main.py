60e9d0941e8d40efa953ed52888350991fb237eb
gaiyadev <gaiyaobed94@gmail.com>
2021-02-16 23:11:28 +0100

    adjustment

--- practice/main.py
+++ practice/main.py
@@ -1,16 +1,24 @@
 score = int(input("Please Enter your score: "))
+scoreArr = []
 
 if(score >= 70 and score <= 100):
-    print("Excellent, you got A "+ score)
-elif (score >= 60 and score< 70):
+    print("Excellent, you got A ", score)
+    scoreArr.append(score)
+elif (score >= 60 and score < 70):
+    scoreArr.append(score)
     print("You got B", score)
 elif (score >= 50 and score < 60):
+    scoreArr.append(score)
     print("You got C", score)
-elif (score >= 40 and score< 50): 
+elif (score >= 40 and score < 50): 
+    scoreArr.append(score)
     print("You got D", score)
-elif (score >= 35 and score <40):
+elif (score >= 35 and score < 40):
+    scoreArr.append(score)
     print("You got", score)
 elif(score >=0 and score < 35):
+    scoreArr.append(score)
     print("You got F", score)
 else :
     print("invalid")
+print(scoreArr)
